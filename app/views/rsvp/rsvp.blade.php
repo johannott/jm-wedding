e54399042646d96b8cff6b6f6dad5202a0ac197a
Johann Ott <johann.ott@gmail.com>
2014-10-06 22:49:46 +0100

    Improve the RSVP form UI - Added yes/no radio and veg check

--- app/views/rsvp/rsvp.blade.php
+++ app/views/rsvp/rsvp.blade.php
@@ -1,19 +1,54 @@
 {{ Form::open(array('url'=>'/post')) }}
 
-     <h3>RSVP</h3>
+    <h3>Maeve and Johann</h3>
+    <h3>December 28 2014</h3>
 
-      <h5>Tell us if your coming!</h5>
+    <p>Kindly RSVP <strong class="underline">each</strong> guest separately by Nov 30 so we can properly accomodate everyone on this special day.</p>
 
-     <ul>
+    <p>With all our love, M & J</p>
+
+    <ul>
         @foreach($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
     </ul>
 
-    {{ Form::text('guest1', null, array('placeholder'=>'First Guest Name', 'class' => 'johann')) }}
-    {{ Form::text('guest2', null, array('placeholder'=>'Second Guest Name')) }}
-    {{ Form::text('email', null, array('placeholder'=>'Email Address')) }}
-    {{ Form::password('password', array('placeholder'=>'Password')) }}
+    <div class="row">
+               <div class="large-12 columns">
+                    <p>Everything is set! Tell us if your coming!</p>
+                </div>
+                <div class="large-12 columns">
+                  {{ Form::radio('rsvp', 'yes'); }}
+                  {{ Form::label('rsvp', 'Yes, would not miss it!', array('class' => 'yesorno')) }}
+                </div>
+                 <div class="large-12 columns">
+                    {{ Form::radio('rsvp', 'no'); }}
+                    {{ Form::label('rsvp', 'Sorry, will not make it', array('class' => 'yesorno')) }}
+                 </div>
+     </div>
 
-    {{ Form::submit('RSVP', array('class'=>'rsvpButton')) }}
+   <div class="row">
+        <div class="large-12 columns">
+                {{ Form::checkbox('vegetarian', 'value', false, array('id'=>'vegetarian')) }}
+                {{ Form::label('vegetarian', 'Tick if you would you like the Vegetarian option?', array('class' => 'vegetarian')) }}
+        </div>
+    </div>
+
+     <div class="row">
+        <div class="large-6 columns">
+            {{ Form::text('guest1', null, array('placeholder'=>'First Guest Name', 'class' => 'johann')) }}
+        </div>
+     </div>
+    <div class="row">
+        <div class="large-6 columns">
+            {{ Form::text('email', null, array('placeholder'=>'Email Address (Optional)')) }}
+        </div>
+     </div>
+    <div class="row">
+        <div class="large-6 columns">
+            {{ Form::password('password', array('placeholder'=>'Password')) }}
+        </div>
+     </div>
+
+    {{ Form::submit('Send RSVP', array('class'=>'rsvpButton')) }}
 {{ Form::close() }}
